131050ebd4020097704df9b1d420006c4a5ce3f8
Ian <bontaq@gmail.com>
2024-05-24 23:04:39 -0400

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'wouter';
+import Landing from './pages/landing/Page';
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route path="/" component={Landing} />
+      </Switch>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
---
+++ src/pages/landing/Page.tsx
@@ -0,0 +1,7 @@
+const Page = () => {
+  return (
+    <h3>Hello</h3>
+  )
+}
+
+export default Page;
